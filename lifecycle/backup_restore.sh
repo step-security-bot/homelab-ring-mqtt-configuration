0888cbe9c83a1581c8cbd8bf1ace726c65610134
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2023-06-21 15:32:05 +0200

    fix: add recursive flag to download

--- lifecycle/backup_restore.sh
+++ lifecycle/backup_restore.sh
@@ -34,7 +34,7 @@ function restore() {
 
   # download backup from S3
   echo "downloading and restoring storage..."
-  s3cmd --access_key=${GCS_ACCESS_KEY_ID} --secret_key="${GCS_SECRET_ACCESS_KEY}" --host="https://storage.googleapis.com" --host-bucket="https://storage.googleapis.com" --force get s3://${S3_ASSETS_BUCKET_PATH}/ ${DATA_PATH}/
+  s3cmd --access_key=${GCS_ACCESS_KEY_ID} --secret_key="${GCS_SECRET_ACCESS_KEY}" --host="https://storage.googleapis.com" --host-bucket="https://storage.googleapis.com" --recursive --force get s3://${S3_ASSETS_BUCKET_PATH}/ ${DATA_PATH}/
 }
 
 function copy_configuration() {
